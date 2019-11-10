3f77307e8b3cf6bee746844e7460adc370cebfeb
Ginni Kakkar <ginnikakkar20@gmail.com>
2019-11-10 12:00:14 -0500

    Updating code to read comments from JSON files and give output in JSON files

--- src/main.py
+++ src/main.py
@@ -3,6 +3,11 @@ __author__ = 'user'
 #from src 
 import sentiment
 import re
+import json
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+import numpy
 
 def extract_video_id(url):
     pattern1  = "^https://www\.youtube\.com/watch\?v=.*$"
@@ -19,22 +24,47 @@ if __name__ == "__main__":
 
     filepath = 'video_ids.txt'
     output_file = 'output_analysis.txt'
-    with open(filepath,'r') as fp:
-        youtube_url = fp.readline()
-        cnt = 1
-        while youtube_url != '':
-            print(cnt,"------url--------", youtube_url,"\n")
-            video_id = extract_video_id(youtube_url)
-            comments_ratio = 100
+    DATA_PATH = 'Comments_Youtube'
+    FILE = "Output/Output.%s.json"
 
-            if video_id is None:
-                print("Invalid URL")
+    comment_files = [join(DATA_PATH, f) for f in listdir(DATA_PATH) if (isfile(join(DATA_PATH, f)) and "json" in f)]
+    #print(comment_files)
 
-            else:
-                result, dictionary_comments = sentiment.get_sentiment(video_id,comments_ratio)
-                #print("comments are------------------------", dictionary_comments)
-                with open(output_file, 'a') as f:
-                    print(youtube_url,":\n", dictionary_comments,"\n", file=f)
-            youtube_url = fp.readline()
-            print("next youtube url#########", youtube_url)
-            cnt += 1
+    for f in comment_files:
+        #print(f)
+        with open(f, 'r') as outfile:
+            data = json.load(outfile)
+            data = pd.DataFrame(data)
+            #print("----data-----",data)
+            #print("-------2==-------", data['text'])
+            dictionary_comments = sentiment.get_sentiment(data['text'],100)
+            print("-----dictionary----", dictionary_comments)
+
+            video_id = f[26:37]
+            #print("-----vid is----", video_ids)
+            with open(FILE % video_id, "w") as f:
+                print("------output file now--------------")
+                f.write(json.dumps(dictionary_comments))
+
+    #data['author_replied'] = data['author_replied'].astype(int)
+
+
+    # with open(filepath,'r') as fp:
+    #     youtube_url = fp.readline()
+    #     cnt = 1
+    #     while youtube_url != '':
+    #         print(cnt,"------url--------", youtube_url,"\n")
+    #         video_id = extract_video_id(youtube_url)
+    #         comments_ratio = 100
+
+    #         if video_id is None:
+    #             print("Invalid URL")
+
+    #         else:
+    #             dictionary_comments = sentiment.get_sentiment(video_id,comments_ratio)
+    #             #print("comments are------------------------", dictionary_comments)
+    #             with open(output_file, 'a') as f:
+    #                 print(youtube_url,":\n", dictionary_comments,"\n", file=f)
+    #         youtube_url = fp.readline()
+    #         print("next youtube url#########", youtube_url)
+    #         cnt += 1
--- src/sentiment.py
+++ src/sentiment.py
@@ -9,7 +9,7 @@ import youtube_stats
 import pickle
 
 
-def calculate_score(pos,neg,likes,dislikes,comments_ratio):
+def calculate_score(pos,neg,comments_ratio):
     pos_percent = None
     neg_percent = None
 
@@ -26,44 +26,46 @@ def calculate_score(pos,neg,likes,dislikes,comments_ratio):
         pos_percent = (pos/(pos+neg))*100
         neg_percent = 100-pos_percent
 
-    likes_percent = (likes/(likes+dislikes))*100
-    dislikes_percent = 100 - likes_percent
+    # likes_percent = (likes/(likes+dislikes))*100
+    # dislikes_percent = 100 - likes_percent
 
     pos_percent = pos_percent * (comments_ratio/100)
     neg_percent = neg_percent * (comments_ratio/100)
 
-    likes_dislikes_ratio = 1-(comments_ratio/100)
+    # likes_dislikes_ratio = 1-(comments_ratio/100)
 
-    likes_percent *= likes_dislikes_ratio
-    dislikes_percent *= likes_dislikes_ratio
+    # likes_percent *= likes_dislikes_ratio
+    # dislikes_percent *= likes_dislikes_ratio
 
-    pos_sentiment = pos_percent + likes_percent
-    neg_sentiment = neg_percent + dislikes_percent
+    pos_sentiment = pos_percent
+    neg_sentiment = neg_percent
 
     #print(pos_sentiment," - ",neg_sentiment)
 
     if pos_sentiment > 45 and pos_sentiment < 60:
-        return "mixed"
+        return "mixed", pos_sentiment
     elif pos_sentiment >= 60:
-        return "positive"
+        return "positive", pos_sentiment
     else:
-        return "negative"
+        return "negative", pos_sentiment
 
 
 def bag_of_words(words):
     return dict([word, True] for word in words)
 
-def get_sentiment(video_id,comments_ratio):
+def get_sentiment(comments,comments_ratio):
     #comments = youtube_comments.get_youtube_comments(video_id)
-    comments = download_comment.commentExtract(video_id)
+    #comments = download_comment.commentExtract(video_id)
     #print("---------comments extracted--------", comments)
     pos = 0
     neg = 0
-
+    print("############################IN HERE###################")
     positive_comments = []
     negative_comments = []
-
+    mixed_comments = []
+    #print("-----comments----", comments)
     for com in comments:
+        #print("---EACH COM------",com)
         filtered_comments = filter.filter_comments(com)
 
         training.train_classifier()
@@ -80,18 +82,26 @@ def get_sentiment(video_id,comments_ratio):
             else:
                 neg += 1
 
-        if (pos - neg) > 0:
+        # if (pos - neg) > 0:
+        #     positive_comments.append(com)
+        # else:
+        #     negative_comments.append(com)
+
+        result, score = calculate_score(pos,neg,comments_ratio)
+        if(result == "positive"):
             positive_comments.append(com)
-        else:
+        if(result == "negative"):
             negative_comments.append(com)
+        if(result == "mixed"):
+            mixed_comments.append(com)
 
-    no_of_likes , no_of_dislikes = youtube_stats.get_likes_dislikes(video_id)
-
+    #no_of_likes , no_of_dislikes = youtube_stats.get_likes_dislikes(video_id)
+    print("-----##########---------")
     #print(no_of_likes," - ",no_of_dislikes)
-    dictionary_comments = {'Positive Comments': positive_comments, 'Negative Comments': negative_comments}
+    dictionary_comments = {'Positive Comments': positive_comments, 'Negative Comments': negative_comments, 'Mixed Comments': mixed_comments}
 
-    result = calculate_score(pos,neg,no_of_likes,no_of_dislikes,comments_ratio)
+    #overall_result = calculate_score(pos,neg,no_of_likes,no_of_dislikes,comments_ratio)
 
-    return result,dictionary_comments
+    return dictionary_comments
 
 
